fb0a0fc7d419754f168bb06513c497bb9df35604
Guangting Yu <yugtmath@umich.edu>
2018-09-27 00:48:04 -0400

    545 hw2 p4 handwritten partition

--- Homework/hw2/problem4/Handwritten_digit_classification.m
+++ Homework/hw2/problem4/Handwritten_digit_classification.m
@@ -2,3 +2,11 @@ load mnist_49_3000
 [d,n] = size(x);
 i = 1; % index of image to be visualized
 imagesc(reshape(x(:,i),[sqrt(d),sqrt(d)])')
+
+%% Partition %%
+partition = 2000;
+x_train = x(:,1:partition);
+x_test = x(:,partition+1:end);
+y_train = y(1:partition);
+y_test = y(partition+1:end);
+
