68cd1546a2d17b5c469e0d80561f968bc932f9a3
Guangting Yu <yugtmath@umich.edu>
2018-09-27 23:38:48 -0400

    545 hw2 p4 newton iteration

--- Homework/hw2/problem4/Handwritten_digit_classification.m
+++ Homework/hw2/problem4/Handwritten_digit_classification.m
@@ -10,3 +10,39 @@ x_test = x(:,partition+1:end);
 y_train = y(1:partition);
 y_test = y(partition+1:end);
 
+x_train = [ones(1,size(x_train,2));x_train];
+y_train(y_train<0)=0;
+
+%% Newton-Raphson iteration %%
+w=zeros(size(x_train,1),1);
+lambda=10;
+for i=1:500
+    gradient = 2*lambda*w+x_train*(y_train-sigmoid(x_train, -w))';
+    p=sigmoid(x_train,w);
+    W=diag(p.*(1-p));
+    hessian=2*lambda*size(x_train,1)*eye(size(w,1))+x_train*W*x_train';
+    w_new=w-hessian\gradient;
+    if vecnorm(w_new-w)/vecnorm(w)<0.001
+        w=w_new;
+        disp('converge with objective function')
+        J=(y_train-1).*log(sigmoid(x_train,-w))-...
+            y_train.*log(sigmoid(x_train,w));
+        disp(sum(J)+lambda*(w'*w))
+        break;
+    else
+        w=w_new;
+    end
+end
+
+%% Testing %%
+predict=sigmoid([ones(1,size(x_test,2));x_test],-w);
+compare=ones(size(predict));
+compare(predict<0.5)=-1;
+disp('test error')
+disp(1-nnz(y_test==compare)/size(y_test,2))
+
+
+
+function[output] = sigmoid(x, theta)
+output = 1./ (1+exp(-theta'*x));
+end
