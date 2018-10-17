dbdc60693358e816d286045ef610038f8dd0e3e1
Guangting Yu <yugtmath@umich.edu>
2018-10-17 18:58:19 -0400

    545 hw3 p4b

--- Homework/hw3/problem4/subgradient.m
+++ Homework/hw3/problem4/subgradient.m
@@ -1,20 +1,45 @@
 load('nuclear.mat')
 
-loss_hinge=@(y,t)max(0,1-y.*t);
-
-% loss_hinge([0,1,2],[3,4,5])
-
-rng(0)
-x=[ones(1,size(x,2));x];
-x=x';
-y=y';
-lambda=0.001;
-theta=zeros(1,size(x,2));
-
+w=[0.5;0.5]; b=0;
 J=zeros(1000,1);
 for j=1:1000
-    u=lambda*theta-y'*x/2;
-    theta=theta-u*100/j;
-    J(j)=sum(loss_hinge(y,x*theta'))/size(y,1)+lambda*(vecnorm(theta(2:end))^2)/2;
+    u=zeros(size(w,1)+1,1);
+    for i=1:size(x,2)
+        u=u+subgradient_hinge(w,b,x,y,i);
+    end
+    b=b-u(1)*100/j;
+    w=w-u(2:end)*100/j;
+    J(j)=objectiveJ(w,b,x,y);
 end
-plot(J)
+
+scatter(x(1,y==-1),x(2,y==-1),'d')
+hold on
+scatter(x(1,y==1),x(2,y==1))
+
+x1=0:0.02:8;
+x2=(-b-w(1)*x1)/w(2);
+plot(x1,x2,'g')
+
+set(gcf,'Units','inches');
+screenposition = get(gcf,'Position');
+set(gcf,...
+    'PaperPosition',[0 0 screenposition(3:4)],...
+    'PaperSize',[screenposition(3:4)]);
+print -dpdf -painters hw3p4b1
+hold off
+loglog(J)
+xlabel('iteration')
+ylabel('objective function')
+print -dpdf -painters hw3p4b2
+
+function[J]=objectiveJ(w,b,x,y)
+hinge_loss=@(y,t) max(0,1-y.*t);
+J=sum(hinge_loss(y,w'*x+b))/size(y,2);
+J=J+0.001*(w'*w)/2;
+end
+
+function[ui]= subgradient_hinge(w,b,x,y,i)
+ui=zeros(size(w,1)+1,1);
+ui(1)=(sign(w'*x(:,i)+b-y(i))-y(i))/(2*size(y,2));
+ui(2:end)=x(:,i)*ui(1)+0.001*w/size(y,2);
+end
