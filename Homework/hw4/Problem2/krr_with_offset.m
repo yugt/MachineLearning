18ff04f3fe5d89a095c92d4b2d4077c738ddda0c
Yu <yugtmath@umich.edu>
2018-10-22 21:01:49 -0400

    545 hw4 p2 compile pass but prediction wrong

--- Homework/hw4/Problem2/krr_with_offset.m
+++ Homework/hw4/Problem2/krr_with_offset.m
@@ -3,3 +3,20 @@ load('../../hw3/problem1/bodyfat_data.mat')
 n=150;
 X_train=X(1:n,:);
 y_train=y(1:n);
+X_test=X(n+1:end,:);
+y_test=y(n+1:end);
+
+[m,d]=size(X_test);
+
+sigma=15;
+lambda=0.003;
+K=exp(-dist2(X_train,X_train)/(2*sigma^2));
+K_tilde=K-K*ones(n)-ones*K+ones(n)*K*ones(n);
+K_prime=exp(-dist2(X_test,X_train)/(2*sigma^2));
+
+J=eye(n)-ones(n)/n;
+
+
+Core=J*(eye(n)-(n*lambda*eye(n)+K_tilde)\K_tilde)*J;
+y_predict=K_prime*Core*y_train/(n*lambda)...
+    +ones(m,n)*(eye(n)-K*Core/(n*lambda))*y_train/n;
