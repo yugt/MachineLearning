8e8e98db3283409824a0ba1b1adc44b785948616
Guangting Yu <yugtmath@umich.edu>
2018-11-03 13:58:03 -0400

    545 hw4 p2 bug-free in all versions of KRR prediction

--- Homework/hw4/Problem2/krr_with_offset.m
+++ Homework/hw4/Problem2/krr_with_offset.m
@@ -15,12 +15,15 @@ K_tilde=K-K*ones(n)/n-ones(n)*K/n+ones(n)*K*ones(n)/n^2;
 K_prime=exp(-dist2(X_test,X_train)/(2*sigma^2));
 
 J=eye(n)-ones(n)/n;
+y_tilde=J*y_train;
 
-Core=J*((K_tilde+n*lambda*eye(n))\J);
-y_predict=K_prime*Core*y_train...
-    +ones(m,n)*(eye(n)-K*Core)*y_train/n;
-
+Core=J*((K_tilde+n*lambda*eye(n))\y_tilde);
+y_predict=K_prime*Core+ones(m,n)*(y_train-K*Core)/n;
 (y_test-y_predict)'*(y_test-y_predict)/m
+
+% Core=J*((K_tilde+n*lambda*eye(n))\J);
+% y_predict=K_prime*Core*y_train...
+%     +ones(m,n)*(eye(n)-K*Core)*y_train/n;
 
 % Core=J*(eye(n)-(n*lambda*eye(n)+K_tilde)\K_tilde)*J;
 % y_predict=K_prime*Core*y_train/(n*lambda)...
