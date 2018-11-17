f6927d868236dfcd366a4c953758e9e95766f3ca
Yu <yugtmath@umich.edu>
2018-11-17 16:16:03 -0500

    545 hw5 p3 initial code corrected

--- Homework/hw5/problem3/mlr.m
+++ Homework/hw5/problem3/mlr.m
@@ -1,31 +1,22 @@
-clear all
 close all
 rng(0);
 n = 200; % sample size
 K = 2; % number of lines
-e
-w
-b
-v
-=
-=
-=
-=
-[.7
-[-2
-[.5
-[.2
-.3]; % mixing weights
-1]; % slopes of lines
--.5]; % offsets of lines
-.1]; % variances
+e=[.7 .3]; % mixing weights
+w=[-2 1]; % slopes of lines
+b=[.5 -.5]; % offsets of lines
+v=[.2 .1]; % variances
+
+x=zeros(n,1);
+y=zeros(n,1);
+
 for i=1:n
-x(i) = rand;
-if rand < e(1);
-y(i) = w(1)*x(i) + b(1) + randn*sqrt(v(1));
-else
-y(i) = w(2)*x(i) + b(2) + randn*sqrt(v(2));
-end
+    x(i) = rand;
+    if rand < e(1)
+        y(i) = w(1)*x(i) + b(1) + randn*sqrt(v(1));
+    else
+        y(i) = w(2)*x(i) + b(2) + randn*sqrt(v(2));
+    end
 end
 plot(x,y,'bo')
 hold on
