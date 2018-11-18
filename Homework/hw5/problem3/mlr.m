70c510a96efb98c1c8e12cfca5c6d6977054a74e
Guangting Yu <yugtmath@umich.edu>
2018-11-18 13:37:28 -0500

    545 hw5 p3 writeup

--- Homework/hw5/problem3/mlr.m
+++ Homework/hw5/problem3/mlr.m
@@ -29,24 +29,28 @@ w=[1 -1];
 b=[0 0];
 v=repmat(var(y),1,2);
 
-% x: n*d
-% w: d*K
+% x: n*d;  w: d*K
 phi=@(y,x,w,b,v)...
-normpdf(y*ones(size(y,2),K),...
-    x*w+ones(n,size(b,1))*b,...
+normpdf(y*ones(size(y,2),K),x*w+b,...
     ones(n,size(v,1))*sqrt(v));
-ephi=@(e,y,x,w,b,v)...
-(ones(n,size(e,1))*e).*phi(y,x,w,b,v);
 
 % \frac{\sum_{l=1}^n \gamma_{lk}^{(j)}x_l}
 % {\sum_{l=1}^n \gamma_{lk}^{(j)}}
 tilde=@(g,z)repmat(z,[1 K])-...
 repmat((sum(g.*(z*ones(size(z,2),K)),1)./sum(g,1)),[n 1]);
 
-for i=1:10000
+Q=zeros(n,1);
+Q(1)=-Inf;
+for i=1:size(Q,1)
 % E-step
-gamma=ephi(e,y,x,w,b,v);
+gamma=e.*phi(y,x,w,b,v);
 gamma=gamma./sum(gamma,2);
+
+% check stop criteria
+Q(i+1)=sum(sum(gamma.*log(e.*phi(y,x,w,b,v))));
+if Q(i+1)-Q(i)<1e-4
+    break
+end
 
 % M-step
 e=sum(gamma,1)/n;
@@ -58,6 +62,26 @@ b=sum(gamma.*(y-x*w),1)./sum(gamma,1);
 v=sum(gamma.*(y-(x*w+b)).^2,1)./sum(gamma,1);
 end
 
-t=0:0.01:1;
-plot(t,w(1)*t+b(1),'r')
-plot(t,w(2)*t+b(2),'r')
+% plot
+plot(t,w(1)*t+b(1),'.')
+plot(t,w(2)*t+b(2),'.')
+
+set(gcf,'Units','inches');
+screenposition = get(gcf,'Position');
+set(gcf,...
+    'PaperPosition',[0 0 screenposition(3:4)],...
+    'PaperSize',[screenposition(3:4)]);
+print -dpdf -painters hw5p3a -r300
+
+clf
+plot(Q(1:i));
+xlabel('iteration');
+ylabel('log-likelihood')
+
+set(gcf,'Units','inches');
+screenposition = get(gcf,'Position');
+set(gcf,...
+    'PaperPosition',[0 0 screenposition(3:4)],...
+    'PaperSize',[screenposition(3:4)]);
+print -dpdf -painters hw5p3b -r300
+close all
